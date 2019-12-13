273cc5eb9cf6f8c4016e8a2093165f9cff74d4b9
jymun <34541795+jymun@users.noreply.github.com>
2019-12-13 16:55:03 +0900

    Update lamp.tf

--- Azure/terraform/hcl/lamp/lamp.tf
+++ Azure/terraform/hcl/lamp/lamp.tf
@@ -168,7 +168,7 @@ resource "azurerm_virtual_machine" "web" {
   location              = "${var.azure_region}"
   resource_group_name   = "${azurerm_resource_group.default.name}"
   network_interface_ids = ["${azurerm_network_interface.web.id}"]
-  vm_size               = "Standard_A2"
+  vm_size               = "D2s_v3"
   tags                  = "${module.camtags.tagsmap}"
 
   storage_image_reference {
@@ -207,7 +207,7 @@ resource "azurerm_virtual_machine" "web-alternative" {
   location              = "${var.azure_region}"
   resource_group_name   = "${azurerm_resource_group.default.name}"
   network_interface_ids = ["${azurerm_network_interface.web.id}"]
-  vm_size               = "Standard_A2"
+  vm_size               = "D2s_v3"
   tags                  = "${module.camtags.tagsmap}"
 
   storage_image_reference {
